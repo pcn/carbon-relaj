29e79d490951724b3c0e4c259beec260afcf9191
Peter C. Norton <pn@knewton.com>
2013-12-23 00:39:39 -0500

    Small test for the file opening.

--- test/carbon_relaj/core_test.clj
+++ test/carbon_relaj/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [carbon-relaj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-make-file-name
+  (testing "Make-file-name"
+    (let [dir-name "/tmp/foo/temp"
+          thread-id (.getId (Thread/currentThread))
+          the-time (carbon-relaj.core/make-time-map)
+          time-float-str (str (the-time :seconds) "." (format "%03d" (the-time :ms))) ]
+      (is (= (carbon-relaj.core/make-file-name dir-name thread-id the-time)
+             (str dir-name "/"  thread-id "-" time-float-str))))))
+
+; (deftest a-test
+;   (testing "FIXME, I fail."
+;     (is (= 0 1))))
