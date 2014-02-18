d6b5d6c7a9baf1b576505803ec3334cb60100ee0
Peter C. Norton <pn@knewton.com>
2014-02-18 02:58:08 -0500

    The jvm check+exit is in util now.

--- src/carbon_relaj/util.clj
+++ src/carbon_relaj/util.clj
@@ -12,3 +12,14 @@
   ([message errorcode]
      (error message)
      (System/exit (int errorcode))))
+
+
+;; Check that we're using a jvm that gives fs the ability to do
+;; hard links.
+(defn check-jvm-version []
+  (if (< 1 (count (filter true? (clojure.core/map #(= "link" (str (first %)))
+                                                  (seq (ns-publics (the-ns 'me.raynes.fs)))))))
+    (exit-error
+     (str "This jre doesn't provide you with the ability to do hard links.  Use java versions >=1.7.\n"
+          "Exiting with a sad face.  :(\n")
+     100)))
